b784126d4110db6e2ccd8a6e0ae9b292091db389
Harshit Patle <harshitpatle1530@gmail.com>
2026-07-18 13:03:54 +0530

    feat(presets): add password type preset with configurable strength requirements

---
+++ presets/password.js
@@ -0,0 +1,23 @@
+export const passwordPreset = {
+    disable: [
+        "lowVowelRatio",
+        "symbolOnly",
+        "allCaps",
+        "unicodeOnly"
+    ],
+    enable: ["passwordStrength"],
+    rules: {
+        minLength: { minLength: 8 },
+        repeatedChar: { threshold: 4 },
+        keyboardPattern: { minLength: 4 },
+        entropy: { minLength: 8, minEntropy: 2.5 },
+        passwordStrength: {
+            minLength: 8,
+            requireUppercase: true,
+            requireLowercase: true,
+            requireNumber: true,
+            requireSpecial: true,
+            checkCommon: true
+        }
+    }
+};
